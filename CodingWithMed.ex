793fc5b2fa4081adb10af52f7d9cfdbddace839d
foshesss <foshessss@gmail.com>
2022-03-15 20:17:08 -0400

    pattern matching + silencing params

---
+++ CodingWithMed.ex
@@ -0,0 +1,23 @@
+defmodule M do
+
+    defp print_num(9001), do: IO.puts("ITS OVAR 9000")
+    defp print_num(n) when rem(n, 2) == 0, do: IO.puts("Divisible by 2")
+    defp print_num(_n), do: IO.puts("Not divisible by 2")
+
+    def print("Mederick") do
+        print_num(3)
+        IO.puts("FUCK YOU!")
+    end
+
+    def print(name) do
+        IO.puts("Hello " <> name <> "!")
+    end
+
+    def add_list([]), do: 0
+    def add_list([head | tail]) do
+        head + add_list(tail)
+    end
+end
+
+ #IO.puts(M.add_list([1, 2, 3, 4, 5]))
+[1, 2, 3, 4, 5] |> M.add_list |> IO.puts
