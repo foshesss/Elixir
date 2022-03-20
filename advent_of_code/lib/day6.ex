8131b65cfdc1206962c7502f14d785ac081fab5c
foshesss <foshessss@gmail.com>
2022-03-19 22:09:22 -0400

    my thoughts about day6

--- advent_of_code/lib/day6.ex
+++ advent_of_code/lib/day6.ex
@@ -1,3 +1,5 @@
+# easily the best problem so far. super cool and fun to optimize
+
 defmodule Day6 do
   def parse(file_name) do
     file_name
@@ -9,19 +11,20 @@ defmodule Day6 do
     end)
   end
 
+  # need queue because offset for newborns to have kids is 8 days. 
+  # they'll have a kid on day (birthday + 2), adjusting overflow where 6 -> 0
   defp next_day(birthdays, -1, _i, queue), do: Enum.sum(birthdays) + Enum.sum(queue)
   defp next_day(birthdays, day, i, [to_be_born|tail]) do
     newborns = Enum.at(birthdays, i)
 
     birthdays
-    |> List.replace_at(i, newborns + to_be_born)
+    |> List.replace_at(i, newborns + to_be_born) # remove first from queue
     |> next_day(
       day - 1, 
       (if i + 1 > 6, do: 0, else: i + 1), 
-      tail ++ [newborns]
+      tail ++ [newborns] # add newborns to end of queue
       )
   end
-
 
   def part1(input) do
     List.foldl(input, [0,0,0,0,0,0,0], fn elem, base ->
